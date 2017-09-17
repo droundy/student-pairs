402682e1f086c2367f50ed2921eb6815cf15b650
David Roundy <roundyd@physics.oregonstate.edu>
2017-09-17 05:12:37 -0700

    cut unused package

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-import 'package:path_provider/path_provider.dart';
 import 'package:share/share.dart';
 
 final GoogleSignIn _googleSignIn = new GoogleSignIn();
